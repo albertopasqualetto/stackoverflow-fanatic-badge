1f510037d4ced702bfdfbbb6e2f40685c8d2d2fa
Alberto Pasqualetto <39854348+albertopasqualetto@users.noreply.github.com>
2024-02-16 16:40:50 +0100

    Handle async

--- tg_send.py
+++ tg_send.py
@@ -14,17 +14,19 @@ logging.config.fileConfig('logging.conf')
 
 test_msg = '*test*'
 
-
-async def send(msg, chat_id=os.environ.get('TG_USER_ID'), token=os.environ.get('TG_BOT_TOKEN'), notification=True):
+def send(msg, chat_id=os.environ.get('TG_USER_ID'), token=os.environ.get('TG_BOT_TOKEN'), notification=True):
 	"""
 	Send a message to a telegram user specified on chatId
 	chat_id must be a number!
 	"""
 	logging.info("Sending message to " + chat_id)
+	asyncio.run(send_async(msg, chat_id, token, notification))
+
+async def send_async(msg, chat_id, token, notification):
 	bot = telegram.Bot(token=token)
 	async with bot:
 		await bot.send_message(chat_id=chat_id, text=msg, disable_notification=(not notification), parse_mode=telegram.constants.ParseMode.MARKDOWN_V2)
 
 
 if __name__ == '__main__':
-	asyncio.run(send(test_msg))
+	send(test_msg)
