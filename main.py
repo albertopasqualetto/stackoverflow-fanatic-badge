f4eb078f3bc99814264b220e61110ff8f49cdbfa
Alberto Pasqualetto <39854348+albertopasqualetto@users.noreply.github.com>
2024-02-20 22:34:19 +0100

    Fixed markdown V2 problems

--- main.py
+++ main.py
@@ -31,22 +31,22 @@ def main(tries=0):
 			time.sleep(100)
 			main(tries + 1)
 		else:
-			logging.error("Tried to login 10 times and failed!")
-			send("*Tried to login 10 times and failed!*\nYou have *NOT* accessed Stack Overflow in the last 24 hours!\nLast login: " + str(datetime.fromtimestamp(last_access)) + "\nSuccess: " + success_print)
+			logging.error("Tried to login 10 times and failed")
+			send(text = "*Tried to login 10 times and failed*\nYou have *NOT* accessed Stack Overflow in the last 24 hours\nLast login: " + telegram.helpers.escape_markdown(str(datetime.fromtimestamp(last_access)), version=2) + "\nSuccess: " + success_print)
 	elif not log_return[0]:
 		logging.error("Failed to login, retrying... Tries: " + str(tries))
 		if tries < 10:
 			time.sleep(100)
 			main(tries + 1)
 		else:
-			logging.error("Tried to login 10 times and failed but last access was less than 24 hours ago!")
-			send("*Tried to login 10 times and failed!*\nYou have *NOT* accessed Stack Overflow *now*!" + "\nSuccess: " + success_print + "\nLast login: " + str(datetime.fromtimestamp(last_access)) + "\nTries: " + str(tries))
+			logging.error("Tried to login 10 times and failed but last access was less than 24 hours ago")
+			send("*Tried to login 10 times and failed!*\nYou have *NOT* accessed Stack Overflow *now*" + "\nSuccess: " + success_print + "\nLast login: " + telegram.helpers.escape_markdown(str(datetime.fromtimestamp(last_access)), version=2) + "\nTries: " + str(tries))
 	elif log_return[1] < 100 or log_return[1] % 30 == 0:
-		send("You have accessed Stack Overflow in the last 24 hours!\nConsecutive days: " + str(log_return[1]) + "\nLast login: " + str(datetime.fromtimestamp(last_access)) + "\nSuccess: " + success_print, notification=False)
+		send("You have accessed Stack Overflow in the last 24 hours\nConsecutive days: " + str(log_return[1]) + "\nLast login: " + telegram.helpers.escape_markdown(str(datetime.fromtimestamp(last_access)), version=2) + "\nSuccess: " + success_print, notification=False)
 	elif log_return[1] == 100:
-		send("\U0001F389 You have accessed Stack Overflow for 100 consecutive days!\n<del>Now you can stop me, I will send a notification every 30 days</del>", notification=True)
+		send("\U0001F389 You have accessed Stack Overflow for 100 consecutive days\!\n~Now you can stop me, I will send a notification every 30 days~", notification=True)
 
-	logging.info("Done!")
+	logging.info("Done")
 
 
 if __name__ == "__main__":
